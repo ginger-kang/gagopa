8e31a4e30cac5d1cd2b479a679bff22a69508a9c
ginger-kang <kdhoon07@gmail.com>
2020-11-22 01:42:20 +0900

    :sparkles: amplify frontend setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
