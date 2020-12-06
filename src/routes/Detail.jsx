8f94f9f608bd871fafae55b5d093762185dc302d
ginger-kang <kdhoon07@gmail.com>
2020-12-06 22:58:58 +0900

    :sparkles: get pictures using graphql in Detail components

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { getPicture } from '../graphql/queries';
 
 const Detail = ({ match }) => {
   console.log(match.params.cityName);
   console.log(match.params.id);
+  const [pictureObj, setPictureObj] = useState(null);
+
+  useEffect(() => {
+    fetchPictures();
+  }, []);
+
+  const fetchPictures = async () => {
+    try {
+      const data = await API.graphql(
+        graphqlOperation(getPicture, {
+          id: match.params.id,
+        }),
+      );
+      const picture = data.data.getPicture;
+      setPictureObj(picture);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return <div>detail</div>;
 };
 
