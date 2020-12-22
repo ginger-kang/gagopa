f6efb1c9acedf0828c02ad060bc1da4ad079c211
ginger-kang <kdhoon07@gmail.com>
2020-12-23 00:42:09 +0900

    :sparkles: getting user object in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { GlobalStyle } from './global-styles';
 import Router from './Router';
 import { useDarkMode } from './hooks/useDarkMode';
 import { lightTheme, darkTheme } from './theme';
+import { Auth } from 'aws-amplify';
 
 export const ThemeContext = createContext({
   theme: darkTheme,
@@ -11,12 +12,26 @@ export const ThemeContext = createContext({
 
 const App = () => {
   const [theme, toggleTheme] = useDarkMode();
+  const [userObj, setUserObj] = useState(null);
+
+  useEffect(() => {
+    try {
+      Auth.currentUserInfo().then((user) => {
+        if (user) {
+          console.log(user);
+          setUserObj(user);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <React.Fragment>
         <GlobalStyle theme={theme === lightTheme ? lightTheme : darkTheme} />
-        <Router />
+        <Router userObj={userObj} />
       </React.Fragment>
     </ThemeContext.Provider>
   );
