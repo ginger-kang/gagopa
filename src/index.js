cf82a9e9abd78e06d8aea56b98539905d053f153
ginger-kang <kdhoon07@gmail.com>
2020-12-29 01:44:54 +0900

    :wrench: handle many URLs

--- src/index.js
+++ src/index.js
@@ -2,8 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+import config from './aws-exports';
+
+const urlsIn = config.oauth.redirectSignIn.split(',');
+const urlsOut = config.oauth.redirectSignOut.split(',');
+const oauth = {
+  domain: config.oauth.domain,
+  scope: config.oauth.scope,
+  redirectSignIn: config.oauth.redirectSignIn,
+  redirectSignOut: config.oauth.redirectSignOut,
+  responseType: config.oauth.responseType,
+};
+const hasLocalhost = (hostname) =>
+  Boolean(
+    hostname.match(/localhost/) ||
+      hostname.match(/127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/),
+  );
+const hasHostname = (hostname) =>
+  Boolean(hostname.includes(window.location.hostname));
+const isLocalhost = hasLocalhost(window.location.hostname);
+if (isLocalhost) {
+  urlsIn.forEach((e) => {
+    if (hasLocalhost(e)) {
+      oauth.redirectSignIn = e;
+    }
+  });
+  urlsOut.forEach((e) => {
+    if (hasLocalhost(e)) {
+      oauth.redirectSignOut = e;
+    }
+  });
+} else {
+  urlsIn.forEach((e) => {
+    if (hasHostname(e)) {
+      oauth.redirectSignIn = e;
+    }
+  });
+  urlsOut.forEach((e) => {
+    if (hasHostname(e)) {
+      oauth.redirectSignOut = e;
+    }
+  });
+}
+const configUpdate = config;
+configUpdate.oauth = oauth;
+Amplify.configure(configUpdate);
 
 ReactDOM.render(
   <React.StrictMode>
