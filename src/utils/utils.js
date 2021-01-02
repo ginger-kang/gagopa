23498c551b76b74fd88892b2f952a2c0ce287fe6
ginger-kang <kdhoon07@gmail.com>
2021-01-03 03:31:58 +0900

    :page_facing_up: add city list to translate kor

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,4 +5,10 @@ export const cityToKo = {
   kyoto: '교토',
   enosima: '에노시마',
   yamanashi: '야마나시',
+  yokohama: '요코하마',
+  okinawa: '오키나와',
+  nagoya: '나고야',
+  odaiba: '오다이바',
+  hukuoka: '후쿠오카',
+  kobe: '고베',
 };
